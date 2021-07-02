1130f5b10a6eb083b0d416acc390293fb3e02e80
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-07-02 19:37:20 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -1,5 +1,3 @@
-
-
 import os
 from telethon import events, Button, custom
 from userbot.thunderconfig import Config
@@ -9,23 +7,23 @@ from userbot import ALIVE_NAME, bot
 currentversion = "2.1"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Kakashi UserBot"
 ASSIS_PIC = os.environ.get("ASSIS_PIC", None)
 if ASSIS_PIC is None:
-     PM_IMG = "https://telegra.ph/file/beb843ce962a738c52cad.jpg"
+     PM_IMG = "https://telegra.ph/file/f98d138354df7d2f914de.jpg"
 else:
      PM_IMG = ASSIS_PIC
 
 
-pm_caption = " ►**ASSISTANT IS:** `ONLINE`\n\n"
-pm_caption += "► **SYSTEMS STATS**\n"
-pm_caption += "► **Telethon Version:** `1.15.0` \n"
-pm_caption += f"► **Assistant Version** : `{currentversion}`\n"
-pm_caption += f"► **My Master** : {DEFAULTUSER} \n"
-pm_caption += "► **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "► **Copyright** : [Black Lighting](GitHub.com/keinshin/Black-Lightning)\n"
-light = [[Button.url("✧Repos✧", "https://github.com/KeinShin/Black-Lightning"), Button.url("✧Support✧", "https://t.me/lightning_support_group")]]
-light +=[[custom.Button.inline("✧Help✧", data="gibcmd")]]
+pm_caption = "      **Assɪsᴛᴀɴᴛ ɪs** `Oɴʟɪɴᴇ`\n\n"
+pm_caption += "         **Sʏsᴛᴇᴍ Sᴛᴀᴛs**n"
+pm_caption += "• **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ**► `1.15.0` \n"
+pm_caption += f"• **Assɪsᴛᴀɴᴛ Vᴇʀsɪᴏɴ**► `{currentversion}`\n"
+pm_caption += f"• **Mʏ Pᴇʀᴏᴏ Mᴀsᴛᴇʀ** ► {DEFAULTUSER} \n"
+pm_caption += "• **Lɪᴄᴇɴᴄᴇ** ► [UserBot](https://github.com/ProBoy-X/Kakashi-UserBot/blob/master/LICENSE)\n"
+pm_caption += "• **Cᴏᴘʏʀɪɢʜᴛ** ► [Kakashi UB](GitHub.com/ProBoy-X/Kakashi-userbot)\n"
+light = [[Button.url("✨Rᴇᴘᴏ✨", "https://github.com/ProBoy-X/Kakashi-userbot"), Button.url("🤓Sᴜᴘᴘᴏʀᴛ🤓", "https://t.me/Kakashi_support")]]
+light +=[[custom.Button.inline("✧Hᴇʟᴘ✧", data="gibcmd")]]
 @tgbot.on(events.NewMessage(pattern="^/alive" , func=lambda e: e.sender_id == bot.uid))
 async def _(event):
     await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption, buttons=light)
