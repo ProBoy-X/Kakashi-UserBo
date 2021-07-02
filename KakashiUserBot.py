5f0c19e499842cf105fa07219e78e1361f198b6a
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-07-02 02:44:39 -0700

    Okk

---
+++ KakashiUserBot.py
@@ -0,0 +1,3 @@
+import os
+os.system("git clone https://github.com/ProBoy-X/Kakashi-UserBot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
+ 
--- lightningrun.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("git clone https://github.com/KeinShin/Black-Lightning /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
