699ef9d50d50cf09a64fbe4e843594ac4e30b3be
Jass Manak <82396523+JassManak1125@users.noreply.github.com>
2021-07-03 10:47:24 +0530

    Update lightning.py

--- userbot/lightning.py
+++ userbot/lightning.py
@@ -5,18 +5,18 @@
 #chala jaa bhosdike 
 import os
 from userbot import ALIVE_NAME 
-MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "KAKASHI-USERBOT"
 NAME = os.environ.get("BOT_NICK_NAME")
-BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
+BOT =  = str(NAME) if NAME else "KAKASHI-USERBOT"
 #op for keinshin
 
-Keinshin = "[KEINSHIN](https://t.me/keinshin)"
-OP = "[BLACK LIGHTNING](https://github.com/KeinShin/Black-Lightning)"
-OKAY = "[SUPPORT GROUP](https://t.me/LIGHTINGSUPPORT)"
+Jass Manak = "[Jass Manak](https://t.me/Its_JassManak)"
+OP = "[Kakashi-UserBot](https://github.com/ProBoy-X/Kakashi-UserBot)"
+OKAY = "[Support Group](https://t.me/Kakashi_Support)"
 #itna test h aur aage krte h
 #test successful raha ab aage 
-ALIVE = "BLACK LIGHTNING BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
-USERBOT = " HELLO MASTER MY NAME IS BLACK LIGHTNING BOT I AM A BEST USERBOT 💝"
+ALIVE = "KAKASHI USERBOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for black lightning
+USERBOT = " HELLO MASTER MY NAME IS KAKASHI.. A POWERFUL USERBOT MADE WITH💝"
 EMOJI = "⚡"
 #yrr isko apne bot me aply krne se pehle mere se pooch lena ok
 #aur aage add kruga abhi busy okay 🤔
